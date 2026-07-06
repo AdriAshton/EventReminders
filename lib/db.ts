f6c3181bec513023a2ba8d63634ae12c0021f8e4
AdriAshton <adrian.q.ashton@gmail.com>
2026-07-05 21:33:50 -0400

    Fix Vercel deployment

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,9 @@
 import { Pool } from 'pg';
 
+const connectionString = process.env.DATABASE_URL ?? 'postgresql://postgres:password@localhost:5432/birthday_reminder';
+
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'birthday_reminder',
-  password: 'password',
-  port: 5432, // default PostgreSQL port
+  connectionString,
 });
 
 export default pool;
